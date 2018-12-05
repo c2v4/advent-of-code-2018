cb13fe9cb227157843f9ee9ad8c4986cf82e0ff4
c2v4 <the_pro@wp.pl>
2018-12-05 21:13:06 +0100

    Day 4 Plus

---
+++ src/test/kotlin/com/c2v4/advent18/Day4PlusTest.kt
@@ -0,0 +1,28 @@
+package com.c2v4.advent18
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day4PlusTest{
+
+    @Test
+    internal fun sleepyGuardTest() {
+        Assertions.assertThat(sleepyFrequentGuard("[1518-11-01 00:00] Guard #10 begins shift\n" +
+            "[1518-11-01 00:05] falls asleep\n" +
+            "[1518-11-01 00:25] wakes up\n" +
+            "[1518-11-01 00:30] falls asleep\n" +
+            "[1518-11-01 00:55] wakes up\n" +
+            "[1518-11-01 23:58] Guard #99 begins shift\n" +
+            "[1518-11-02 00:40] falls asleep\n" +
+            "[1518-11-02 00:50] wakes up\n" +
+            "[1518-11-03 00:05] Guard #10 begins shift\n" +
+            "[1518-11-03 00:24] falls asleep\n" +
+            "[1518-11-03 00:29] wakes up\n" +
+            "[1518-11-04 00:02] Guard #99 begins shift\n" +
+            "[1518-11-04 00:36] falls asleep\n" +
+            "[1518-11-04 00:46] wakes up\n" +
+            "[1518-11-05 00:03] Guard #99 begins shift\n" +
+            "[1518-11-05 00:45] falls asleep\n" +
+            "[1518-11-05 00:55] wakes up")).isEqualTo(4455)
+    }
+}
